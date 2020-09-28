7f9c9d2107f099b400291f1b2b8d4a982a27af7d
qwe <1477961757@qq.com>
2020-09-28 14:41:20 +0800

    HelloGit

---
+++ src/main/java/cn/kgc/git/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.kgc.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("Hello Git!");
+	}
+
+}
