e751bade45472b859987df555c8b1492c72c4e42
qwe <1477961757@qq.com>
2020-09-28 14:43:07 +0800

    123123ggdd

--- src/main/java/cn/kgc/git/HelloGit.java
+++ src/main/java/cn/kgc/git/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
             System.out.println("Hello Git!");
+            System.out.println("123123");
+            System.out.println("ggdd");
 	}
 
 }
